0c7699134349bd57d890304288599a02e6ca5f41
Fotis Chrysoulas <f.chrysoulas@epsilon-singularlogic.eu>
2023-01-12 12:42:34 +0200

    Session 4

---
+++ Session-04/Session-04/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_04 {
+
+    public class ProgramOne {
+
+
+        public string HelloName() {
+
+            string hello = "Hello";
+            string name = "Fotis";
+
+            return hello + " " + name;
+
+        }
+
+    }
+}
+
+
--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Session_04;
+using System.ComponentModel;
+
+internal class Program {
+
+    private static void Main(string[] args) {
+    
+        Console.WriteLine("Hello, World!");
+
+        // local (object) 
+        //Square mySq = new Square();
+
+        //string functionDescription = "the surface is : " + mySq.Surface();
+
+        //Console.WriteLine(functionDescription);
+
+        ////int i = Convert.ToInt32("1");
+
+        //decimal j = (5 + 6) / 2;
+
+        
+
+
+        // 1. Write a C# program to print Hello and your name in the same line. 
+        string hello = "Hello";
+        string name = "Fotis";
+
+        Console.WriteLine(hello + " " + name);
+
+        ProgramOne programOne = new ProgramOne();
+        Console.WriteLine(programOne.HelloName());
+
+        Console.ReadLine();
+
+    }
+}
