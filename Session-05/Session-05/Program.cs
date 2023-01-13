9a946e4471707f51f7f0a512fc955c4d96b5fe8e
Fotis Chrysoulas <f.chrysoulas@epsilon-singularlogic.eu>
2023-01-13 10:49:13 +0200

    test

---
+++ Session-05/Session-05/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program {
+
+    private static void Main(string[] args) {
+
+        Console.WriteLine("Hello, World!");
+
+        Console.ReadLine();
+        
+        
+        //int j = "";
+    }
+}
