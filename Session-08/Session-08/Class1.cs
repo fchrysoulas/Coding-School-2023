7b7ba565996fc8bc4820ec80078b63bdcb386fb9
Fotis Chrysoulas <f.chrysoulas@epsilon-singularlogic.eu>
2023-01-19 09:14:57 +0200

    test

---
+++ Session-08/Session-08/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_08 {
+
+    public abstract class Shape {
+
+        // abstract method
+        public abstract decimal Surface();
+
+    }
+
+
+    public abstract class Circle : Shape {
+
+        public abstract decimal Perimeter();
+
+
+        //public override decimal Surface() {
+
+        //    return 0;
+        //}
+    }
+
+
+    public class FotisCircle : Circle {
+
+        public override decimal Perimeter() {
+
+            return 0;
+        }
+
+        public override decimal Surface() {
+
+            return 0;
+        }
+    }
+
+
+}
