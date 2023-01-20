62d28feef4ab55688bd456c8e8fe1a958f07bfba
Fotis Chrysoulas <f.chrysoulas@epsilon-singularlogic.eu>
2023-01-20 10:45:05 +0200

    s1

---
+++ Session-10/Session-10/Libs/University.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_10.Libs {
+
+
+    public class Institute {
+
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+
+        public Institute() {
+            ID = Guid.NewGuid();    
+        }
+    }
+
+    public class University : Institute {
+
+       
+        public List<Student> Students { get; set; }
+
+        public University() {
+            Students = new List<Student>();
+        }
+
+    }
+
+    public class Student {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
